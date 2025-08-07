9b43e6c5fd1b8d13f929d33f9594f28007a2f030
srikar appalaraju <forwards2097@gmail.com>
2025-08-06 23:34:52 -0700

    Create version management system

---
+++ thinking_sdk_client/_version.py
@@ -0,0 +1,12 @@
+"""Version information for ThinkingSDK client."""
+
+__version__ = "0.1.0"
+__version_info__ = (0, 1, 0)
+
+def get_version():
+    """Return the version string."""
+    return __version__
+
+def get_version_info():
+    """Return version as a tuple of integers."""
+    return __version_info__
